cda2abbf71ab18c80c708113c47dcef0c44c92a4
hasherezade <hasherezade@gmail.com>
2019-11-14 18:49:24 +0100

    [BUGFIX] Fixed killing the process

--- process_util.cpp
+++ process_util.cpp
@@ -104,15 +104,16 @@ bool kill_till_dead(HANDLE &proc)
         GetExitCodeProcess(proc, &exit_code);
         if (exit_code == STILL_ACTIVE) {
             TerminateProcess(proc, 0);
-        }
-        if (GetLastError() == ERROR_ACCESS_DENIED) {
-            std::cerr << "Could not kill the process: access denied!" << std::endl;
-            break; //process is elevated, cannot kill it:
+            if (GetLastError() == ERROR_ACCESS_DENIED) {
+                std::cerr << "Could not kill the process: access denied!" << std::endl;
+                break; //process is elevated, cannot kill it:
+            }
         }
         else {
             is_killed = true;
             break;
         }
+
     } while (true);
     return is_killed;
 }
