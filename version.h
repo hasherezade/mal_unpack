c08a8e3246600407b938343b4e45b706b9a35a73
hasherezade <hasherezade@gmail.com>
2021-12-23 07:58:41 -0800

    [VERSION] 0.8.6

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define VER_FILEVERSION             0,8,5,0
-#define VER_FILEVERSION_STR         "0.8.5-d\0"
+#define VER_FILEVERSION             0,8,6,0
+#define VER_FILEVERSION_STR         "0.8.6\0"
