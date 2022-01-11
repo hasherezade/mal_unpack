145232cd93d0e5fe4829e6aced56190daadd3a49
hasherezade <hasherezade@gmail.com>
2022-01-10 16:52:37 -0800

    [FEATURE] Enable cache by default

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define VER_FILEVERSION             0,9,0,0
-#define VER_FILEVERSION_STR         "0.9.0\0"
+#define VER_FILEVERSION             0,9,1,0
+#define VER_FILEVERSION_STR         "0.9.1\0"
