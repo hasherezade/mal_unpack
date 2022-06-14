9a06e7b32e1ef9cd2c076ffb053d3a3294b5d4b0
hasherezade <hasherezade@gmail.com>
2022-06-14 07:28:48 -0700

    [VERSION] 0.9.5

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define VER_FILEVERSION             0,9,4,0
-#define VER_FILEVERSION_STR         "0.9.4-a\0"
+#define VER_FILEVERSION             0,9,5,0
+#define VER_FILEVERSION_STR         "0.9.5\0"
